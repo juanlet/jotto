a461ed974a557b02e539ea54bbb0fd26d532de94
Juan Letamendia <juan.letamendia@fundkers.com>
2019-03-11 05:43:54 -0300

    added tests for redux

--- src/Input/Input.js
+++ src/Input/Input.js
@@ -3,14 +3,33 @@ import { connect } from 'react-redux';
 
 class Input extends Component {
     render() {
-      return <div>
-          <button>Hey</button>
-          </div>;
+       const content =  this.props.success ?
+       <div data-test="guess-success">
+       
+       </div>
+       :
+       <form className="form-inline">
+            <input data-test="input-box" 
+                   className="mb-2 mx-sm-3"
+                   id="word-guess"
+                   type="text"
+                   placeholder="enter guess"/>
+            <button data-test="submit-box" 
+                    type="submit" 
+                    className="btn btn-primary mb-2"
+                     >Submit
+            </button>
+        </form>;
+
+      return <div data-test="component-input">
+        
+          {content}
+            </div>;
     }
 }
-
-const mapStateToProps = (state) => {
-    return {};
+ 
+const mapStateToProps = ({success}) => {
+    return {success};
 }
 
 export default connect(mapStateToProps)(Input)
--- src/Input/Input.test.js
+++ src/Input/Input.test.js
@@ -5,7 +5,7 @@ import  {Provider} from 'react-redux';
 import { findByTestAttr, storeFactory } from '../../test/testUtils';
 import Input from './Input';
 
-
+//CONNECTED COMPONENT
 //testing redux component
 const setup = (initialState={}) => {
     const store = storeFactory(initialState);
@@ -15,38 +15,59 @@ const setup = (initialState={}) => {
  const wrapper = shallow(
     <Input store={store} />
     ).dive();
-
+    return wrapper;
 }
 
 
 describe('render', () => {
     describe('word has not been guessed', () => {
          
+        let wrapper;
+
+        beforeEach(() => {
+          const initialState = { success: false};
+          wrapper = setup(initialState);    
+        });
+
         test('renders component without error', () => {
-            setup();   
+             const component = findByTestAttr(wrapper,'component-input');
+             expect(component.length).toBe(1); 
         });
 
         test('renders input box', () => {
-            
+            const inputBox = findByTestAttr(wrapper,'input-box');
+            expect(inputBox.length).toBe(1); 
+
         });
 
         test('renders submit button', () => {
-            
+            const submitButton = findByTestAttr(wrapper,'submit-box');
+            expect(submitButton.length).toBe(1); 
+
         });
 
     });
 
     describe('word has been guessed',() => {
+        let wrapper;
+
+        beforeEach(() => {
+            const initialState = { success: true};
+            wrapper = setup(initialState);
+        });
         test('renders component without error', () => {
-            
+            const component = findByTestAttr(wrapper,'guess-success');
+            expect(component.length).toBe(1);
         });
 
         test('does not render input box', () => {
-            
+            const component = findByTestAttr(wrapper,'input-success');
+            expect(component.length).toBe(0);
         });
 
         test('does not render submit button', () => {
-            
+            const component = findByTestAttr(wrapper,'submit-box');
+            expect(component.length).toBe(0);
         });
 
     });
