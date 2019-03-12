ad50fc9b26ef4e0bb93bf714de1d81435d3892bb
Juan Letamendia <juan.letamendia@fundkers.com>
2019-03-12 03:23:50 -0300

    setup integration test

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import success from './successReducer';
+import guessedWordsReducer from './guessedWordsReducer';
 
 export default combineReducers({
   success,
+  guessedWordsReducer
 });
